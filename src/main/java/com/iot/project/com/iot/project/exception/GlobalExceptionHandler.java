82ac3c607bdfa7787ec75f86fef94e2630503d3d
calderonpablo123 <calderonpablo@gmail.com>
2025-03-31 16:29:10 -0300

    Repositories

---
+++ src/main/java/com/iot/project/com/iot/project/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.iot.project.com.iot.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
---
+++ src/main/java/com/iot/project/com/iot/project/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.iot.project.com.iot.project.exception;
+
+public class UnauthorizedException extends RuntimeException{
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
