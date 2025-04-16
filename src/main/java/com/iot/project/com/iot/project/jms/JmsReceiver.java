d63f580d6b8e287e36953cbdb0800c375a0482e9
calderonpablo123 <calderonpablo@gmail.com>
2025-04-16 14:46:34 -0400

    Jms consumer

--- src/main/java/com/iot/project/com/iot/project/jms/JmsReceiver.java
+++ src/main/java/com/iot/project/com/iot/project/jms/JmsReceiver.java
@@ -1,12 +1,16 @@
-/*package com.iot.project.com.iot.project.jms;
+package com.iot.project.com.iot.project.jms;
 
 import com.iot.project.com.iot.project.dto.sensorData.CreateSensorDataRequest;
+import com.iot.project.com.iot.project.entity.SensorDataHeader;
+
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iot.project.com.iot.project.service.SensorDataService;
 
@@ -15,24 +19,27 @@ import jakarta.jms.TextMessage;
 import lombok.extern.log4j.Log4j2;
 
 @Service
-@RequiredArgsConstructor
 @Log4j2
 public class JmsReceiver  {
 	protected final Log logger = LogFactory.getLog(getClass().getName());
 	ObjectMapper mapper = new ObjectMapper();
-	private final SensorDataService sensorDataService;
+	@Autowired SensorDataService sensorDataService;
 	@JmsListener( containerFactory = "jmsTopicContainerFactory", destination = "${mq.destination:HOLAMUNDO}")
 	public void onMessage(Message jmsMessage) {
 		if(jmsMessage instanceof TextMessage)
 		{
 			try {
 				CreateSensorDataRequest receivedData = this.mapper.readValue(((TextMessage) jmsMessage).getText(), CreateSensorDataRequest.class);
-				sensorDataService.createSensorData(receivedData);
-			}catch (Exception e) {
-				logger.error("Can´t parse message", e);
+				SensorDataHeader _response = sensorDataService.createSensorData(receivedData);
+				System.out.println(_response.getId());
+			}catch (JsonParseException e) {
+				logger.error("Can´t parse message 1", e);
+			}
+			catch (Exception e) {
+				logger.error("Can´t parse message 2", e);
 			} 
 		}
 		
 	}
 
-}*/
+}
--- src/main/java/com/iot/project/com/iot/project/service/SensorDataService.java
+++ src/main/java/com/iot/project/com/iot/project/service/SensorDataService.java
@@ -19,6 +19,7 @@ import com.iot.project.com.iot.project.repository.SensorDataHeaderRepository;
 import com.iot.project.com.iot.project.repository.SensorMetricRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.iot.project.com.iot.project.exception.ConstantsExceptions.ENTITY_NOT_FOUND_BY_COMPANY;
 import static com.iot.project.com.iot.project.exception.ConstantsExceptions.INVALID_SENSOR_API_KEY;
@@ -41,7 +42,7 @@ public class SensorDataService {
         Instant to = Instant.ofEpochSecond(Long.parseLong(request.getTo()));
         return sensorDataHeaderRepository.findBySensorIdsAndDateRangeAndCompanyApiKey(companyId, request.getSensorIds(), from, to);
     }
-
+    @Transactional
     public SensorDataHeader createSensorData(CreateSensorDataRequest request) {
         Sensor sensor = sensorService.getSensorBySensorApiKey(request.getApiKey())
                 .orElseThrow(() -> new NotFoundException(INVALID_SENSOR_API_KEY));
