c992a0b6eb4be782f2d344bb92a564b98adebfa6
Sebastian Gallardo <sagallardol@falabella.cl>
2025-04-19 01:12:58 -0400

    cambios

--- src/main/java/com/iot/project/com/iot/project/jms/JmsReceiver.java
+++ src/main/java/com/iot/project/com/iot/project/jms/JmsReceiver.java
@@ -1,8 +1,12 @@
 package com.iot.project.com.iot.project.jms;
 
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+
 import com.iot.project.com.iot.project.dto.sensorData.CreateSensorDataRequest;
 import com.iot.project.com.iot.project.entity.SensorDataHeader;
 
+import jakarta.jms.BytesMessage;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,22 +28,36 @@ public class JmsReceiver  {
 	protected final Log logger = LogFactory.getLog(getClass().getName());
 	ObjectMapper mapper = new ObjectMapper();
 	@Autowired SensorDataService sensorDataService;
-	@JmsListener( containerFactory = "jmsTopicContainerFactory", destination = "tf-mining-01")
+	@JmsListener( containerFactory = "jmsTopicContainerFactory", destination = "tf-minera-01")
 	public void onMessage(Message jmsMessage) {
-		if(jmsMessage instanceof TextMessage)
-		{
-			try {
-				CreateSensorDataRequest receivedData = this.mapper.readValue(((TextMessage) jmsMessage).getText(), CreateSensorDataRequest.class);
-				SensorDataHeader _response = sensorDataService.createSensorData(receivedData);
-				System.out.println(_response.getId());
-			}catch (JsonParseException e) {
-				logger.error("Can´t parse message 1", e);
+		System.out.println("sdfsd");
+		try {
+			if (jmsMessage instanceof TextMessage textMessage) {
+				CreateSensorDataRequest receivedData = mapper.readValue(textMessage.getText(), CreateSensorDataRequest.class);
+				SensorDataHeader response = sensorDataService.createSensorData(receivedData);
+				log.info("📥 SensorDataHeader guardado con ID: {}", response.getId());
+			} else if (jmsMessage instanceof BytesMessage bytesMessage) {
+				String json = leerBytesMessage(bytesMessage);
+				CreateSensorDataRequest receivedData = mapper.readValue(json, CreateSensorDataRequest.class);
+				SensorDataHeader response = sensorDataService.createSensorData(receivedData);
+				log.info("📦 (Bytes) SensorDataHeader guardado con ID: {}", response.getId());
+			} else {
+				log.warn("⚠️ Tipo de mensaje no manejado: {}", jmsMessage.getClass().getName());
 			}
-			catch (Exception e) {
-				logger.error("Can´t parse message 2", e);
-			} 
+		} catch (Exception e) {
+			log.error("❌ Error procesando mensaje", e);
 		}
 		
+	}
+	private String leerBytesMessage(BytesMessage bytesMessage) throws Exception {
+		bytesMessage.reset();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int bytesRead;
+		while ((bytesRead = bytesMessage.readBytes(buffer)) != -1) {
+			outputStream.write(buffer, 0, bytesRead);
+		}
+		return new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
 	}
 
 }
